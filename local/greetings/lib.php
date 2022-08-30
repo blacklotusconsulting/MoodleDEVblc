1f4a97662fd89f2a106d20ab6bd9a74103029c9a
BlackLotusConsulting <info@blacklotus.eu>
2022-08-30 17:36:22 +0200

    Changes - Moodle Academy Course 3008 Add plugin link to navigation

--- local/greetings/lib.php
+++ local/greetings/lib.php
@@ -23,6 +23,11 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
+/**
+ * Show greetings message.
+ *
+ * @param $user is current user variable.
+ */
 function local_greetings_get_greeting($user) {
     if ($user == null) {
         return get_string('greetinguser', 'local_greetings');
@@ -40,4 +45,36 @@ function local_greetings_get_greeting($user) {
     }
 
     return get_string($langstr, 'local_greetings', fullname($user));
+}
+
+/**
+ * Insert a link to index.php on the site front page navigation menu.
+ *
+ * @param navigation_node $frontpage Node representing the front page in the navigation tree.
+ */
+function local_greetings_extend_navigation_frontpage(navigation_node $frontpage)
+{
+    $frontpage->add(
+            get_string('pluginname', 'local_greetings'),
+            new moodle_url('/local/greetings/index.php'),
+            navigation_node::TYPE_CUSTOM,
+            null,
+            null,
+            new pix_icon('t/message', '')
+    );
+}
+/**
+ * Insert a link to index.php on the site global navigation menu.
+ *
+ * @param global_navigation $root Node representing the global nav page in the navigation tree.
+ */
+function local_greetings_extend_navigation(global_navigation $root)
+{
+    $node = navigation_node::create(
+            get_string('pluginname', 'local_greetings'),
+            new moodle_url('/local/greetings/index.php')
+    );
+
+    $node->showinflatnavigation = true;
+    $root->add_node($node);
 }
